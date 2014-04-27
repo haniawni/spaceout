cec9fe73f83f688edb34fcaaaa72fa59320a28e1
Kevin Mershon <kevin@slixbits.com>
2014-04-27 10:10:54 -0700

    Implement HTTP client connection to server with hard-coded IP for now

--- android/src/main/java/com/spaceout/NeuralAlertnessService.java
+++ android/src/main/java/com/spaceout/NeuralAlertnessService.java
@@ -1,7 +1,13 @@
 package com.spaceout;
 
+import java.io.IOException;
+
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 
 import android.app.Service;
 
@@ -14,6 +20,9 @@ import android.util.Log;
 
 public class NeuralAlertnessService extends Service {
     private Timer refreshTimer;
+    private HttpClient client;
+
+    private String ipAddress = "192.168.1.169";
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -25,16 +34,47 @@ public class NeuralAlertnessService extends Service {
     public void onCreate() {
         super.onCreate();
 
+        this.client = new HttpClient();
         this.refreshTimer = new Timer();
         this.refreshTimer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
-                // TODO - poll server to check if the user has spaced out
                 Log.d(
-                    "SPACE OUT: NeuralAlertnessService",
+                    "SPACEOUT",
                     "polling for alertness..."
                 );
+                checkIsBored();
             }
         }, 0, 1000);
+    }
+
+    // returns true if the user is bored, false otherwise
+    public boolean checkIsBored() {
+        // TODO -- http request to server for boredom
+        HttpMethod method = new PostMethod(
+            "http://" + this.ipAddress + ":8080/neural"
+        );
+
+        try {
+            this.client.executeMethod(method);
+            byte[] responseBody = method.getResponseBody();
+            method.releaseConnection();
+            if (responseBody == null) {
+                Log.d("SPACEOUT", "no response received");
+                // TODO -- show error on android GUI
+                return false;
+            }
+
+            String response = new String(responseBody);
+            Log.d("SPACEOUT", "response was: " + response);
+
+            // TODO -- parse JSON and check "spaced out?" key for true/false
+        } catch (IOException ex) {
+            // TODO -- show error on android GUI
+            Log.e("SPACEOUT", "Failed to connect to server!");
+            Log.e("SPACEOUT", ex.getMessage());
+        }
+
+        return false;
     }
 
     @Override
