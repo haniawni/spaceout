4861a4bf6a252e3561abf39e7d8f85e33fb9597f
Brent Houghton <brent@slixbits.com>
2014-04-26 20:21:06 -0700

    Remove the AudioRecorderService and replace it with the Android class that already implements its functionality--AudioRecord.

--- android/src/main/java/com/spaceout/MainActivity.java
+++ android/src/main/java/com/spaceout/MainActivity.java
@@ -6,12 +6,37 @@ import android.content.Intent;
 
 import android.os.Bundle;
 
+import android.media.MediaRecorder;
+import android.media.AudioRecord;
+
 public class MainActivity extends Activity {
+
+	/**
+	 * The audio sample properties expected by Wit.
+	 */
+	private static final int SAMPLE_RATE_HZ = 8000;
+	private static final int CHANNEL_CONFIG = AudioRecord.CHANNEL_IN_MONO;
+	private static final int AUDIO_FORMAT = AudioRecord.ENCODING_PCM_16BIT;
+	private static final short DURATION_S = 10
+
+    private AudioRecord audioRecorder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        audioRecorder = new AudioRecord(
+			MediaRecorder.AudioSource.MIC,
+			SAMPLE_RATE_HZ,
+			CHANNEL_CONFIG,
+			AUDIO_FORMAT,
+			2 * AudioRecord.getMinBufferSize(
+				SAMPLE_RATE_HZ,
+				CHANNEL_CONFIG,
+				AUDIO_FORMAT
+			)
+		);
     }
 
 }
